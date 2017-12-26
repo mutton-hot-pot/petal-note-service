98186e7638fd0e938c7408e62f390a3362d384c0
Jason Yin <jiangsheng.yin@hp.com>
2017-12-26 18:22:50 +0800

    add create dynamodb config

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const {
   graphql,
@@ -6,71 +6,86 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLNonNull
-} = require('graphql')
-const AWS = require('aws-sdk')
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
+} = require("graphql");
+const uuidv4 = require("uuid/v4");
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
-    name: 'RootQueryType',
+    name: "RootQueryType",
     fields: {
-      retrieveNote: {
-        args: {
-          title: { name: 'title', type: new GraphQLNonNull(GraphQLString) }
-        },
+      retrieveNotes: {
         type: GraphQLString,
-        resolve: (parent, args) => retrieveNote(args.title)
+        resolve: (parent, args) => retrieveNotes()
       }
     }
   }),
   mutation: new GraphQLObjectType({
-    name: 'RootMutationType',
-    createNote: {
-      note: {
-        args: {
-          title: { name: 'title', type: new GraphQLNonNull(GraphQLString) },
-          content: { name: 'content', type: new GraphQLNonNull(GraphQLString) }
-        },
-        type: GraphQLString,
-        resolve: (parent, args) => createNote(args.title, args.content)
+    name: "RootMutationType",
+    fields: {
+      createNote: {
+        note: {
+          args: {
+            title: { name: "title", type: new GraphQLNonNull(GraphQLString) },
+            content: {
+              name: "content",
+              type: new GraphQLNonNull(GraphQLString)
+            }
+          },
+          type: GraphQLString,
+          resolve: (parent, args) => createNote(args.title, args.content)
+        }
       }
     }
   })
-})
+});
 
-const promisify = foo => new Promise((resolve, reject) => {
-  foo((error, result) => {
-    if (error) {
-      reject(error);
-    } else {
-      resolve(result);
-    }
-  })
-})
+const promisify = foo =>
+  new Promise((resolve, reject) => {
+    foo((error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
 
-const retrieveNote = title => promisify(callback =>
-  dynamoDb.get({
-    TableName: process.env.NOTE_TABLE,
-    Key: { title },
-  }, callback))
-  .then((result) => {
-    return result.Item;
-  })
+const retrieveNotes = () =>
+  promisify(callback => {
+    const params = {
+      TableName: process.env.NOTES_TABLE
+    };
+    return dynamoDb.scan(params, callback).then(result => result.Items);
+  });
 
-const createNote = (title, content) => promisify(callback =>
-  dynamoDb.update({
-    TableName: process.env.NOTE_TABLE,
-    Key: { title, content },
-    UpdateExpression: 'ADD title = :title, content = :content',
-    ExpressionAttributeValues: {
-      ':title': title,
-      ':content': content
-    },
-  }, callback))
-  .then(() => nickname);
+const createNote = (title, content) =>
+  promisify(callback => {
+    const params = {
+      TableName: process.env.NOTES_TABLE,
+      Item: {
+        NoteId: { S: uuidv4() },
+        Title: { S: title },
+        Content: { S: content }
+      }
+    };
+    return dynamoDb.update(params, callback).then(result => result.Item);
+  });
 
-module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
-  .then(
-  result => callback(null, { statusCode: 200, body: JSON.stringify(result) }),
-  err => callback(err)
-  )
+const retrieveNote = noteId =>
+  promisify(callback => {
+    const params = {
+      TableName: process.env.NOTES_TABLE,
+      Key: { NoteId: { S: noteId } }
+    };
+    return dynamoDb.get(params, callback).then(result => {
+      return result.Item;
+    });
+  });
+
+module.exports.query = (event, context, callback) =>
+  graphql(schema, event.queryStringParameters.query).then(
+    result => callback(null, { statusCode: 200, body: JSON.stringify(result) }),
+    err => callback(err)
+  );
